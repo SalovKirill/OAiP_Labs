f2f4496b5cac8d82708c310aa5cef08967cd6177
Кирилл <32333692+SalovKirill@users.noreply.github.com>
2017-12-18 22:44:48 +0300

    Laboratornaya#2
    Лабораторная работа по ОАиП №2, Вариант №2

---
+++ Laboratornaya№2/Laboratornaya2.cpp
@@ -0,0 +1,68 @@
+/*
+Салов Кирилл Андреевич
+Лабораторная работа №1
+Вариант №2
+Задание: Написать программу, которая вычисляет среднее арифметическое элементов массива
+без учета минимального и максимального элементов массива. 
+Массив и его длина вводятся пользователем.
+*/
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+int main()
+{
+	printf("Enter length of array\n");
+	int ArrayLength;
+	int result = scanf_s("%d", &ArrayLength);
+	if (result == 0)
+	{
+		printf("Error message\n");
+		_getch();
+		return 0;
+	}
+		printf("Enter Array\n");
+		int Array[1000];
+		int maximum = 0;
+		int minimum = 1000000;
+		int sum = 0;
+		int numberOfMin = 0;
+		int numberOfMax = 0;
+		for (int i = 0; i < ArrayLength; i++)
+	{
+		int result = scanf_s("%d", &Array[i]);
+		if (result == 0)
+		{
+			printf("Error message\n");
+			_getch();
+			return 0;
+		}
+		sum += Array[i];
+		if (Array[i] < minimum)
+		{
+			minimum = Array[i];
+			numberOfMin = 1;
+		}
+		else if (Array[i] == minimum)
+			numberOfMin++;
+		if (Array[i] > maximum)
+		{
+			maximum = Array[i];
+			numberOfMax = 1;
+		}
+		else if (Array[i] == maximum)
+			numberOfMax++;
+	}
+	sum -= maximum * numberOfMax + minimum * numberOfMin;
+	if (sum < 0)
+		printf("0");
+	else
+	{
+		float average = sum * 1.0 / (ArrayLength - numberOfMax - numberOfMin);
+		printf("%1.4f\n", average);
+	}
+	_getch();
+	return 0;
+}
